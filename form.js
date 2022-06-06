e7adffa875c3d810232fed6625a99f7d59ae9a35
Ritika Bhosale <54773240+ritikabhosale@users.noreply.github.com>
2022-06-06 17:04:11 +0530

    Stored input by removing last char

--- form.js
+++ form.js
@@ -1,18 +1,24 @@
 const fs = require('fs');
 process.stdin.setEncoding('utf8');
 
+const isNameInvalid = name => {
+  return !/^[a-z]*$/.test(name) || name.length < 5;
+}
+
+const removeLastChar = str => str.slice(0, -1);
+
 const acceptHobbies = (details) => {
   process.stdin.removeAllListeners('close');
   process.stdin.removeAllListeners('data');
   console.log('Please enter your hobbies');
   process.stdin.on('data', (chunk) => {
-    const hobbies = chunk.split(',');
-    details.hobbies = hobbies;
+    let hobbies = removeLastChar(chunk);
+    details.hobbies = hobbies.split(',');
     process.stdin.emit('close');
   });
 
   process.stdin.on('close', () => {
-    console.log(JSON.stringify(details));
+    console.log('Thank you');
     fs.writeFileSync('./details.json', JSON.stringify(details), 'utf8');
   });
 };
@@ -22,7 +28,8 @@ const acceptDob = (details) => {
   process.stdin.removeAllListeners('data');
   console.log('Please enter your dob (yyyy-mm-dd):');
   process.stdin.on('data', (chunk) => {
-    details.dob = chunk;
+    const dob = removeLastChar(chunk);
+    details.dob = dob;
     process.stdin.emit('close');
   });
 
@@ -34,7 +41,8 @@ const acceptDob = (details) => {
 const acceptName = (details) => {
   console.log('Please enter your name:');
   process.stdin.on('data', (chunk) => {
-    details.name = chunk;
+    const name = removeLastChar(chunk);
+    details.name = name;
     process.stdin.emit('close');
   });
 
