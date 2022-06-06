5706020d86da46af632444bdebda241414634e65
Ritika Bhosale <54773240+ritikabhosale@users.noreply.github.com>
2022-06-06 15:36:11 +0530

    Wrote the input data to json file

--- form.js
+++ form.js
@@ -1,40 +1,47 @@
+const fs = require('fs');
 process.stdin.setEncoding('utf8');
 
-const acceptHobbies = () => {
+const acceptHobbies = (details) => {
   process.stdin.removeAllListeners('close');
   process.stdin.removeAllListeners('data');
   console.log('Please enter your hobbies');
   process.stdin.on('data', (chunk) => {
+    const hobbies = chunk.split(',');
+    details.hobbies = hobbies;
     process.stdin.emit('close');
   });
 
   process.stdin.on('close', () => {
-    console.log('Thank you');
+    console.log(JSON.stringify(details));
+    fs.writeFileSync('./details.json', JSON.stringify(details), 'utf8');
   });
 };
 
-const acceptDob = () => {
+const acceptDob = (details) => {
   process.stdin.removeAllListeners('close');
   process.stdin.removeAllListeners('data');
-  console.log('Please enter your dob:');
+  console.log('Please enter your dob (yyyy-mm-dd):');
   process.stdin.on('data', (chunk) => {
+    details.dob = chunk;
     process.stdin.emit('close');
   });
 
   process.stdin.on('close', () => {
-    acceptHobbies();
+    acceptHobbies(details);
   });
 };
 
-const acceptName = () => {
+const acceptName = (details) => {
   console.log('Please enter your name:');
   process.stdin.on('data', (chunk) => {
+    details.name = chunk;
     process.stdin.emit('close');
   });
 
   process.stdin.on('close', () => {
-    acceptDob();
+    acceptDob(details);
   });
 };
 
-acceptName();
+const details = {};
+acceptName(details);
