a7db79f2972f4bb0f6716ab6f3db2a18c1ef6c29
Ritika Bhosale <54773240+ritikabhosale@users.noreply.github.com>
2022-06-08 19:46:37 +0530

    Added tests for form

---
+++ test/testForm.js
@@ -0,0 +1,73 @@
+const { Form } = require('../form');
+const { Field } = require('../src/field');
+const assert = require('assert');
+const { MultiLineField } = require('../src/multiLineField');
+
+describe('Form', () => {
+  it('should return the current field of the form', () => {
+    const nameField = new Field('name', 'Enter name');
+    const form = new Form(nameField);
+    assert.ok(form.currentField().equals(nameField));
+  });
+
+  it('should return the current field prompt of the form', () => {
+    const nameField = new Field('name', 'Enter name');
+    const form = new Form(nameField);
+    assert.equal(form.currentFieldPrompt(), 'Enter name');
+  });
+
+  it('should return the current field prompt of the form', () => {
+    const nameField = new Field('name', 'Enter name');
+    const form = new Form(nameField);
+    assert.equal(form.currentFieldPrompt(), 'Enter name');
+  });
+
+  it('should fill the given response in current field', () => {
+    const nameField = new Field('name', 'Enter name');
+    const form = new Form(nameField);
+    form.fillField('sakshi');
+    const expected = { name: 'sakshi' };
+    assert.deepStrictEqual(form.getResponses(), expected);
+  });
+
+  it('should return true when all fields are filled', () => {
+    const nameField = new Field('name', 'Enter name');
+    const form = new Form(nameField);
+    form.fillField('sakshi');
+    assert.equal(form.isFilled(), true);
+  });
+
+  it('should return true when all fields are filled', () => {
+    const nameField = new Field('name', 'Enter name');
+    const form = new Form(nameField);
+    form.fillField('sakshi');
+    assert.equal(form.isFilled(), true);
+  });
+
+  it('should return false when any field is unfilled', () => {
+    const nameField = new Field('name', 'Enter name');
+    const dobField = new Field('dob', 'Enter dob');
+    const form = new Form(nameField, dobField);
+    form.fillField('sakshi');
+    assert.equal(form.isFilled(), false);
+  });
+
+  it('should return all the responses of each field', () => {
+    const nameField = new Field('name', 'Enter name');
+    const dobField = new Field('dob', 'Enter dob');
+    const form = new Form(nameField, dobField);
+    form.fillField('sakshi');
+    form.fillField('2000-09-21');
+    const expected = { name: 'sakshi', dob: '2000-09-21' }
+    assert.deepStrictEqual(form.getResponses(), expected);
+  });
+
+  it('should return false when any multiLine field is unfilled', () => {
+    const nameField = new Field('name', 'Enter name');
+    const addressField = new MultiLineField('address', ['Enter line 1', 'Enter line 2']);
+    const form = new Form(nameField, addressField);
+    form.fillField('sakshi');
+    addressField.fill('This is line 1');
+    assert.equal(form.isFilled(), false);
+  });
+}); 
